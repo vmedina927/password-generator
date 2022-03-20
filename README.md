c6242c5513a88de115ad49da1d441189c38bdbe7
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-03-20 15:38:38 -0400

    Final changes to script.js and README.md file

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# password-generator
+# Password Generator Challenge
+
+* Click the button to generate a password
+* Prompts will ask for password criteria
+* Prompts will ask for the length of characters of at least 8 characters and no more than 128 characters.
+* Prompts will ask to include lowercase, uppercase, numeric, and/or special characters.
+* After all prompts are answered then a password should generate that meets the criteria.
+* The password will be generated and displayed in an alert or written on the page.
+
+# Task
+
+This week’s Challenge requires you to create an application that an employee can use to generate a random password based on criteria they’ve selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes. This week’s coursework will teach you all the skills you need to succeed in this assignment.
+
+The password can include special characters. If you’re unfamiliar with these, see this list of Password Special Characters from the OWASP Foundation https://www.owasp.org/index.php/Password_special_characters.
+
+# User Story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+# Acceptance Criteria
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+# Mock-Up
 
 ![image](https://user-images.githubusercontent.com/98541583/159175208-f6a35cf7-cd6c-45c9-be70-2b7d34fa0a8a.png)
--- script.js
+++ script.js
@@ -1,8 +1,74 @@
 // Assignment code here
-
+var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`','{', '|', '}', '~', '`'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Function that will give user options and conditional statements which meet requirements in the criteria.
+function questions() {
+  var isValid = false;
+  do {
+    var length = prompt("Choose password length of at least 8 characters and no more than 128 characters");
+    var askNumbers = confirm("Do you want your password to include numbers?");
+    var askSpecial = confirm("Do you want your password to include special characters?");
+    var askUpperCase = confirm("Do you want your password to include uppercase letters?");
+    var askLowerCase = confirm("Do you want your password to include lowercase letters?");
+    
+    var responses = {
+      length: length,
+      askNumbers: askNumbers,
+      askSpecial: askSpecial,
+      askUpperCase: askUpperCase,
+      askLowerCase: askLowerCase,
+      
+    }
+
+    if ((length , 8)||(length > 128))
+    alert("Choose character between 8 and 128");
+    else if ((!askNumbers)&&(!askSpecial)&&(!askUpperCase)&&(!askLowerCase))
+    alert("Must chose at least one character.");
+    else
+    isValid = true;
+
+  }
+  while(!isValid);
+  return responses;
+}
+
+// Function will come together with user response and creates results which will give a strong password.
+function generatePassword() {
+  var passwordOptions = questions();
+  var possibleCombo = [];
+  var finalPassword = "";
+
+  if (passwordOptions.askNumbers) {
+    for (var i of numbers)
+      possibleCombo.push(i);
+  }
+
+  if (passwordOptions.askSpecial) {
+    for (var i of special)
+      possibleCombo.push(i);
+  }
+  
+  if (passwordOptions.askUpperCase) {
+    for (var i of upperCase)
+      possibleCombo.push(i);
+  }
+
+  if (passwordOptions.askLowerCase) {
+    for (var i of lowerCase)
+      possibleCombo.push(i);
+  }
+
+  console.log(finalPassword)
+  
+  
+}
+
 
 // Write password to the #password input
 function writePassword() {
