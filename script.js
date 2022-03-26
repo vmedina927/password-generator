96a69a8d5e1e429e50bcb646211f34744ddd3b51
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-03-26 10:32:45 -0400

    Fixed generate password to be responsive.

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var special = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`','{', '|', '}', '~', '`'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`','{', '|', '}', '~', '`'];
 
 // Function that will give user options and conditional statements which meet requirements in the criteria.
 function questions() {
@@ -13,22 +13,23 @@ function questions() {
   do {
     var length = prompt("Choose password length of at least 8 characters and no more than 128 characters");
     var askNumbers = confirm("Do you want your password to include numbers?");
-    var askSpecial = confirm("Do you want your password to include special characters?");
-    var askUpperCase = confirm("Do you want your password to include uppercase letters?");
     var askLowerCase = confirm("Do you want your password to include lowercase letters?");
+    var askUpperCase = confirm("Do you want your password to include uppercase letters?");
+    var askSpecial = confirm("Do you want your password to include special characters?");
+
     
     var responses = {
       length: length,
       askNumbers: askNumbers,
-      askSpecial: askSpecial,
-      askUpperCase: askUpperCase,
       askLowerCase: askLowerCase,
+      askUpperCase: askUpperCase,
+      askSpecial: askSpecial,
       
     }
 
-    if ((length , 8)||(length > 128))
-    alert("Choose character between 8 and 128");
-    else if ((!askNumbers)&&(!askSpecial)&&(!askUpperCase)&&(!askLowerCase))
+    if (length < 8||length > 128)
+    alert("Choose number between 8 and 128");
+    else if ((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
     alert("Must chose at least one character.");
     else
     isValid = true;
@@ -49,24 +50,34 @@ function generatePassword() {
       possibleCombo.push(i);
   }
 
-  if (passwordOptions.askSpecial) {
-    for (var i of special)
-      possibleCombo.push(i);
-  }
-  
-  if (passwordOptions.askUpperCase) {
-    for (var i of upperCase)
-      possibleCombo.push(i);
-  }
-
   if (passwordOptions.askLowerCase) {
     for (var i of lowerCase)
       possibleCombo.push(i);
   }
 
-  console.log(finalPassword)
+  if (passwordOptions.askUpperCase) {
+    for (var i of upperCase)
+      possibleCombo.push(i);
+  }
+
+  if (passwordOptions.askSpecial) {
+    for (var i of special)
+      possibleCombo.push(i);
+  }
   
-  
+  console.log(possibleCombo);
+
+  // Empty string to be filled based on for loop selecting random characters from the array
+  var finalPassword = ""
+  console.log(passwordOptions.length);
+
+  for (var i= 0; i < passwordOptions.length; i++){
+    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+    
+}
+  console.log(finalPassword);
+
+  return finalPassword;
 }
 
 
